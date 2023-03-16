760213a8c33057059264dbcfaf4138f1918498c7
DarkShadowG17 <rouissiyazh@gmail.com>
2023-03-16 13:15:12 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+/**
+ * main - prints a text using printf 
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
