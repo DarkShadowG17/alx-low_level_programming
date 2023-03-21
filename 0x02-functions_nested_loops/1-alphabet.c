e375a3c8e1eb78bfc291b5f910ba1ebefad9d275
DarkShadowG17 <rouissiyazh@gmail.com>
2023-03-21 13:47:47 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+	{
+		char alp = 'a';
+
+		while (alp <= 'z')
+		{
+			_putchar(alp);
+			apl++;
+		}
+		_putchar('\n);
+	}
