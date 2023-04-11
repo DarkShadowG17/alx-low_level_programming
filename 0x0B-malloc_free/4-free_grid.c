ddc43b8014aa9ac1c64512a808252f3089cae4be
DarkShadowG17 <rouissiyazh@gmail.com>
2023-04-11 07:34:00 -0700

    task4.1

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid _  function that frees a 2 dimensional grid previously created
+ * free_grid -  function that frees a 2 dimensional grid previously created
  * by your alloc_grid function
  * @grid: multidimensional array
  * @height: height
