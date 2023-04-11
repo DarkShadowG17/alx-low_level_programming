cc5df7d17ed103959edff43522d00f31a266c36a
DarkShadowG17 <rouissiyazh@gmail.com>
2023-04-11 07:44:30 -0700

    task6.1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -48,7 +48,7 @@ char **strtow(char *str)
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
-					ch_free_grid(aout, i);
+					fgrid(aout, i);
 					return (NULL);
 				}
 				break;
