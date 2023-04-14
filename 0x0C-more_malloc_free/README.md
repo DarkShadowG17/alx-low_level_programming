b2ad40f54d8d776baa67dc36c3e7f213155e7aac
DarkShadowG17 <rouissiyazh@gmail.com>
2023-04-14 09:37:26 -0700

    task0

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+README file for 0x0C-more_malloc_free
+
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include<stdio.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
